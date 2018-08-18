ec4582620b35d22f4ff3f93f43b0a3a4e18d5456
alberto <aoterolorenzo@gmail.com>
2018-08-18 02:58:57 +0200

    Mongo auth

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -7,7 +7,7 @@ import {share} from 'rxjs/operators';
 @Injectable()
 export class AppService {
 
-  url = window.location.protocol + '://' + window.location.hostname + ':3000/api/v1/resume/alberto';
+  url = 'https://' + window.location.hostname + ':3000/api/v1/resume/alberto';
 
   resume: any;
 
